5f32e2286a34b37cf70af08bc85e6d00c295ebf1
eighty88 <eighty88@users.noreply.github.com>
2021-12-08 11:32:37 +0000

    deploy: 50f6ed1e0ba66930e4e5368a966c8149e51904d4

---
+++ src/components/Welcome.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="welcome">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Welcome',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.welcome {
+  width: 100%;
+  height: 50px;
+  text-align: center;
+  vertical-align: middle;
+  background: #282828;
+}
+</style>
