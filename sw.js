bcffc9b65b4cac5176579c6a8cd98db6646a10a7
eighty88 <eighty88@users.noreply.github.com>
2021-12-08 13:11:38 +0000

    deploy: e2700700e831805119f12f792f7156fd483a12a8

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/vue-jekyll/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20211208.2206';
+const cacheName = 'chirpy-20211208.2211';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
